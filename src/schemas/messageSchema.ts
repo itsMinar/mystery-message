133a44266bd5881dffec297f43bb9a93ebf45af4
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-06-26 23:52:36 +0600

    Fix Validation for Send Message

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,6 +3,6 @@ import { z } from 'zod';
 export const messageSchema = z.object({
   content: z
     .string()
-    .min(10, { message: 'Content must be at least of 10 characters.' })
+    .min(2, { message: 'Content must be at least of 2 characters.' })
     .max(300, { message: 'Content must be no longer than 300 characters.' }),
 });
