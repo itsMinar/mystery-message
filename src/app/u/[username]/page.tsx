00c6dcd6c7b55b109725459d2c25f0f15e2d4e5b
Md. Minar Munshi <minarmunshi46@gmail.com>
2024-06-27 13:33:10 +0600

    Do Some Fixes

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,10 +1,8 @@
-import { findUser } from '@/actions';
 import SendMessageForm from '@/components/forms/SendMessageForm';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import type { Metadata } from 'next';
 import Link from 'next/link';
-import { notFound } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Send Message - Mystery Message',
@@ -19,12 +17,6 @@ type SendMessagePageProps = {
 export default async function SendMessagePage({
   params,
 }: SendMessagePageProps) {
-  const user = await findUser(params.username);
-
-  if (!user) {
-    notFound();
-  }
-
   return (
     <div className="container mx-auto my-8 max-w-4xl rounded bg-white p-6">
       <h1 className="mb-6 text-center text-4xl font-bold">
